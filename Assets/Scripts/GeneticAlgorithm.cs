f7fa98d2616fe8b7d11a61423856e722ed33bfa1
mateus-etto <mateus.etto@gmail.com>
2016-06-24 18:24:50 -0300

    Genetic Algorithm's Selection and Crossover done

--- Assets/Scripts/GeneticAlgorithm.cs
+++ Assets/Scripts/GeneticAlgorithm.cs
@@ -9,7 +9,7 @@ public class GeneticAlgorithm : MonoBehaviour {
     private const int NUMBER_INPUTS = 33;
     private const int NUMBER_OUTPUTS = 12;
 
-    private const int POPULATION_SIZE = 30;
+    private const int POPULATION_SIZE = 40;
 
 
     // Other scripts
@@ -19,6 +19,8 @@ public class GeneticAlgorithm : MonoBehaviour {
         public double[,] chromosome;
         public bool tested = false;
         public float fitness = 0;
+
+        public Unit() {}
 
         public Unit(double[,] chromosomeMatrix) {
             chromosome = chromosomeMatrix;
@@ -32,6 +34,7 @@ public class GeneticAlgorithm : MonoBehaviour {
     }
 
     List<Unit> gamePopulation = new List<Unit>();
+    int generation;
 
     // Auxiliar variables
     int i, j;
@@ -43,8 +46,10 @@ public class GeneticAlgorithm : MonoBehaviour {
 
         // If failed, create
         if (!gamePopupationLoaded)
-            if (CreatePopulation(gamePopulation))
+            if (CreatePopulation(gamePopulation)) {
+                generation = 0;
                 SavePopulation(gamePopulation, "population.data");
+            }
 
         turnManager = GameObject.Find("Main Camera").GetComponent<TurnManager>();
 
@@ -62,6 +67,8 @@ public class GeneticAlgorithm : MonoBehaviour {
                 using (var binaryStream = new BinaryReader(filestream)) {
                     bool testedValue;
                     float fitnessValue;
+                    // Loading the generation information
+                    generation = binaryStream.ReadInt32();
                     // Looking for all units
                     for (int unit = 0; unit < POPULATION_SIZE; unit++) {
                         double[,] chromosomeToLoad = new double[NUMBER_INPUTS, NUMBER_OUTPUTS];
@@ -95,6 +102,8 @@ public class GeneticAlgorithm : MonoBehaviour {
             System.String fileDirectory = @"Assets\SavedData\" + fileName;
             using (var filestream = File.Create(fileDirectory))
             using (var binarystream = new BinaryWriter(filestream)) {
+                // Saving the generation information
+                binarystream.Write(generation);
                 // Saving all units
                 for (int unit = 0; unit < POPULATION_SIZE; unit++) {
                     // Saving the chromosome matrix
@@ -200,7 +209,112 @@ public class GeneticAlgorithm : MonoBehaviour {
     }
 
     private void PrepareNextGeneration() {
-        print("Prepare next generation");
+        print("Preparing next generation");
+        generation++;
+        List<Unit> nextPopulation = new List<Unit>();
+        float crossoverRate = 0.9f;
+        // Generating the next generation (selection + crossover)
+        nextPopulation = Selection(nextPopulation, crossoverRate);
+    }
+
+    private Unit Roulette(List<Unit> population) {
+        float fitnessSum = 0;
+        // SUM of all fitness
+        for (int i = 0; i < population.Count; i++)
+            fitnessSum += population[i].fitness;
+        // Random for selection
+        float rouletteValue = Random.Range(0f, fitnessSum);
+        // Looking for the selected chromosome
+        Unit selectedUnit = null;
+        for (int i = 0; i < population.Count; i++) {
+            rouletteValue -= population[i].fitness;
+            if (rouletteValue <= 0.00001f) {
+                // Found the selected chromosome, transfer it to the new generation
+                selectedUnit = population[i];
+                population.RemoveAt(i);
+                break;
+            }
+        }
+        return selectedUnit;
+    }
+
+    private List<Unit> Selection(List<Unit> nextPopulation, float crossoverRate) {
+        // 1 - Letting all fitness values become a least 0
+        /*float smallestFitness = 1000, biggestFitness = -1000;
+        int biggestFitnessIndex = 0, secondBiggestFitnessIndex = 0;
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            // Finding the smallest fitness
+            if (gamePopulation[i].fitness < smallestFitness)
+                smallestFitness = gamePopulation[i].fitness;
+            // Finding the biggest fitness
+            if (gamePopulation[i].fitness > biggestFitness) {
+                biggestFitness = gamePopulation[i].fitness;
+                secondBiggestFitnessIndex = biggestFitnessIndex;
+                biggestFitnessIndex = i;
+            }
+        }*/
+
+        gamePopulation.Sort((s1, s2) => s2.fitness.CompareTo(s1.fitness));
+        // Adding the smallest fitness on everyone
+        /*if (smallestFitness < 0) {
+            for (int i = 0; i < POPULATION_SIZE; i++)
+                gamePopulation[i].fitness += smallestFitness;
+        }*/
+        if (gamePopulation[POPULATION_SIZE - 1].fitness < 0)
+            for (int i = 0; i < POPULATION_SIZE; i++)
+                gamePopulation[i].fitness -= gamePopulation[POPULATION_SIZE - 1].fitness;
+
+        // 2 - Transfer the 2 best chromosome to the new population (elitism)
+        nextPopulation.Add(gamePopulation[0]);
+        nextPopulation.Add(gamePopulation[1]);
+        //gamePopulation.RemoveAt(biggestFitnessIndex);
+
+        // 3 - Select using the Roulette algorithm, and make the crossover
+        Unit firstParent, secondParent;
+        while (nextPopulation.Count < POPULATION_SIZE) {
+            firstParent = Roulette(gamePopulation);
+            secondParent = Roulette(gamePopulation);
+            // Checking if will happen crossover
+            if (Random.Range(0f, 1f) < crossoverRate) {
+                Unit[] offspring = Crossover(firstParent.chromosome, secondParent.chromosome);
+                nextPopulation.Add(offspring[0]);
+                nextPopulation.Add(offspring[1]);
+            }
+            else { // Won't happen crossover
+                nextPopulation.Add(firstParent);
+                nextPopulation.Add(secondParent);
+            }
+        }
+
+        // 4 - Reset the fitness value of the copied chromosomes
+        /*for (int i = 0; i < nextPopulation.Count; i++)
+            nextPopulation[i].tested = false;*/
+
+        return nextPopulation;
+    }
+
+    private Unit[] Crossover(double[,] firstParent, double[,] secondParent) {
+        Unit[] offspring = new Unit[2];
+        double[,] firstOffspringChromosome = new double[NUMBER_INPUTS, NUMBER_OUTPUTS];
+        double[,] secondOffspringChromosome = new double[NUMBER_INPUTS, NUMBER_OUTPUTS];
+        int i, j;
+        // The Crossover algorithm (Uniform crossover)
+        float exchangeProbability = 0.5f;
+        for (i = 0; i < NUMBER_INPUTS; i++ )
+            for (j = 0; j < NUMBER_OUTPUTS; j++ ) {
+                if (Random.Range(0f, 1f) < exchangeProbability) {
+                    firstOffspringChromosome[i, j] = firstParent[i, j];
+                    secondOffspringChromosome[i, j] = secondParent[i, j];
+                }
+                else {
+                    firstOffspringChromosome[i, j] = secondParent[i, j];
+                    secondOffspringChromosome[i, j] = firstParent[i, j];
+                }
+            }
+        // Putting them on the 'Unit' variable
+        offspring[0] = new Unit(firstOffspringChromosome);
+        offspring[1] = new Unit(secondOffspringChromosome);
+        return offspring;
     }
 
 }
